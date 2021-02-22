3f2f32a4ed1ad367bca2668c6b66e5fe48c856b5
Alexandra Rodriguez-Beuerman <arbeuerman@gmail.com>
2021-02-22 23:14:55 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,7 +15,7 @@ def hopper
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
@@ -36,7 +36,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,8 +55,9 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages].join(" ")
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
@@ -81,8 +82,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:yukihiro_matsumoto] = {
+    known_for: "Ruby", langugages: ["LISP", "C"]
+  }
+  programmer_hash
 end
+
 
 def changing_alan
   # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
@@ -103,7 +108,9 @@ def changing_alan
         :languages => ["C"]
       }
     }
-
+    
+  programmer_hash[:alan_kay][:known_for] = "GUI"
+  programmer_hash
 end
 
 def adding_to_dennis
@@ -125,5 +132,7 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+    
+    programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+    programmer_hash
 end
