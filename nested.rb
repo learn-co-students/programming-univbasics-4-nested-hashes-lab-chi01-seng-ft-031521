26ad06120ea4c8549fd22ca7a2981f79f708cd73
Daniel Lee Beasley <dannyleebeasley@gmail.com>
2021-03-05 18:58:09 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -15,12 +15,10 @@ def hopper
         :languages => ["C"]
       }
     }
-
+programmer_hash[:grace_hopper]
 end
 
 def alan_kay_is_known_for
-	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
-	
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -36,7 +34,7 @@ def alan_kay_is_known_for
         :languages => ["C"]
       }
     }
-
+programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -52,20 +50,13 @@ def dennis_ritchies_language
       },
       :dennis_ritchie => {
         :known_for => "Unix",
-        :languages => ["C"]
+        :languages => "C"
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -81,14 +72,17 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    
+programmer_hash[:yukihiro_matsumoto] = {
+        :known_for => "Ruby",
+        :languages => ["LISP", "C"]
+      }
+      
+programmer_hash
 end
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable. 
-  # return the entire updated hash
-
-	programmer_hash = 
+  programmer_hash = 
  		{
       :grace_hopper => {
         :known_for => "COBOL",
@@ -104,12 +98,11 @@ def changing_alan
       }
     }
 
+programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash
 end
 
 def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
-
 	programmer_hash = 
  		{
       :grace_hopper => {
@@ -125,5 +118,6 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+programmer_hash
 end
